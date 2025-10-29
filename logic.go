1ebb4801375ca1455189c45c17ce996146e53acb
tkdeng <web@tkdeng.com>
2025-10-29 11:10:49 -0400

    updated

--- logic.go
+++ logic.go
@@ -5,6 +5,8 @@ import (
 )
 
 // If will run a bash if [[ logic ]] method, and return true or false to the output for go to process
+//
+// note: uses RunRaw method
 func If(logic string, dir string, env []string) bool {
 	if out, err := RunRaw(`if [[ `+logic+` ]]; then echo "true"; else echo "false"; fi`, dir, env); err == nil {
 		if strings.TrimSpace(string(out)) == "true" {
